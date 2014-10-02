e6336eef9ea38c921a76952ed2310b034d1c5124
Doug Letterman <github@renderfast.com>
2014-10-02 09:00:45 -0700

    Added vertex and fragment shaders.

--- src/TrackLayer.cpp
+++ src/TrackLayer.cpp
@@ -24,12 +24,35 @@ void
 TrackLayer::_setup()
 {
     _shader = new QOpenGLShaderProgram();
-    /*
-    _shader->addShaderFromSourceCode(QOpenGLShader::Vertex, "\
-                                     ");
+    // shaders from http://www.geeks3d.com/20130705/shader-library-circle-disc-fake-sphere-in-glsl-opengl-glslhacker/3/
+    _shader->addShaderFromSourceCode(QOpenGLShader::Vertex,
+        "#version 120\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;\n"
+        "    gl_TexCoord[0] = gl_MultiTexCoord0;\n"
+        "}\n");
     _shader->addShaderFromSourceCode(QOpenGLShader::Fragment, "\
+                                     #version 120
+                                     uniform sampler2D tex0;
+                                     uniform float border_size; // 0.01
+                                     uniform float disc_radius; // 0.5
+                                     uniform vec4 disc_color; // vec4(1.0, 1.0, 1.0, 1.0)
+                                     uniform vec2 disc_center; // vec2(0.5, 0.5)
+                                     void main (void)
+    {
+        vec2 uv = gl_TexCoord[0].xy;
+        
+        vec4 bkg_color = texture2D(tex0,uv * vec2(1.0, -1.0));
+        
+        // Offset uv with the center of the circle.
+        uv -= disc_center;
+        
+        float dist = sqrt(dot(uv, uv));
+        float t = smoothstep(disc_radius+border_size, disc_radius-border_size, dist);
+        gl_FragColor = mix(bkg_color, disc_color, t);
+    }
                                      ");
-    */
     _isSetup = true;
 }
 

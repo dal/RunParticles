c8be8cae049f6915cced797f8ae6736f9cb240fe
Doug Letterman <github@renderfast.com>
2014-10-08 09:15:38 -0700

    Give the DemoPlayer a pointer to the GLWidget. Adding track files to the app bundle as resources.

--- src/DemoPlayer.cpp
+++ src/DemoPlayer.cpp
@@ -6,15 +6,18 @@
 //
 //
 
+#include <QApplication>
 #include <QFile>
 
 #include "DemoPlayer.h"
+#include "GLWidget.h"
 #include "MainWindow.h"
 
 
-DemoPlayer::DemoPlayer(MainWindow *mainWindow) :
+DemoPlayer::DemoPlayer(MainWindow *mainWindow, GLWidget *glWidget) :
     QObject(mainWindow),
     _mainWindow(mainWindow),
+    _glWidget(glWidget),
     _timer(new QTimer(this)),
     _position(0),
     _audioPlayer(new QMediaPlayer(this))
@@ -25,6 +28,14 @@ DemoPlayer::DemoPlayer(MainWindow *mainWindow) :
     _timer->setSingleShot(true);
     _timer->setInterval(1000);
     _timer->start();
+    _copyResources();
+}
+               
+void
+DemoPlayer::_copyResources()
+{
+    QFile::copy(":/music" , "/tmp/tontura.mp3");
+    QFile::copy(":/../resources/map.xml" , "/tmp/map.xml");
 }
 
 void
@@ -34,19 +45,43 @@ DemoPlayer::play()
     switch (_position)
     {
         case 0:
-            _mainWindow->slotMaximizeGLWidget();
+            _glWidget->showMaximized();
+            _glWidget->setFocus();
             _mainWindow->slotPlaybackRateChanged("64x");
             _mainWindow->loadMapFile("/Users/dal/tmp/helloWorld");
             _interval = -1;
             break;
         case 1:
             playMusic();
-            _mainWindow->slotPlay();
+            _glWidget->slotPlay();
+            _interval = 4000;
+            break;
+        case 2:
+            _glWidget->slotLockViewToLayer(4);
+            _glWidget->zoom(0.2);
+            _interval = 8000;
+            break;
+        case 3:
+            _glWidget->slotLockViewToLayer(18);
+            _glWidget->zoom(2.0);
+            _interval = 8000;
+            break;
+        case 4:
+            _glWidget->slotLockViewToLayer(36);
+            _interval = 6000;
+            break;
+        case 5:
+            _glWidget->slotLockViewToLayer(605);
+            _interval = 8000;
+            break;
+        case 6:
+            //qApp->quit();
+            _interval = -1;
             break;
             
     };
     if (_interval > 0)
-        _timer->setInterval(_interval);
+        _timer->start(_interval);
     else
         _timer->stop();
     _position++;
@@ -54,7 +89,6 @@ DemoPlayer::play()
 
 void DemoPlayer::playMusic()
 {
-    QFile::copy(":/music" , "/tmp/tontura.mp3");
     _audioPlayer->setMedia(QUrl::fromLocalFile("/tmp/tontura.mp3"));
     _audioPlayer->setVolume(50);
     _audioPlayer->play();
